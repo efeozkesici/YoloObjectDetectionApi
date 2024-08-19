73b8b2e6a9570125bd6cb40b32c63b1e77b5f7f4
efeozkesici <efealiozkesici@hotmail.com>
2024-08-19 21:51:41 +0300

    upload forgotten test input image

--- sample/test_api.py
+++ sample/test_api.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://localhost:8000/detect/"
 
-files = {'file': open('input.jpg', 'rb')}
+files = {'file': open('input.jpeg', 'rb')}
 
 target_classes = 'person,car'
 data = {'target_classes': target_classes}
